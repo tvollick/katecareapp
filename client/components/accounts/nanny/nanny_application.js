fbd7000da7ad9700c09fe417593bc68c40bbd0f0
Tyler Vollick <Tyler Vollick>
2016-11-28 19:37:25 -0700

    commit before trying to add new collection for application status

--- client/components/accounts/nanny/nanny_application.js
+++ client/components/accounts/nanny/nanny_application.js
@@ -57,7 +57,81 @@ class NannyApplication extends Component {
 								value: "any"
 							}
 						]}
-					/>										
+					/>
+					<InputField 
+						label="Do you have any allergies(dogs, cats, ect.)?"		
+						slug="allergies"
+					/> 
+					<InputField
+						label="What is your desired hourly/salary range?"
+						slug="salary"
+					/>	
+					<RadioField 
+						label="Are you comfortable with light housekeeping and meal prep related to the children?" 
+						slug="housekeeping" 
+						options={[
+							{
+								label: "Yes", 
+								value: 'yes'
+							}, 
+							{
+								label: 'No', 
+								value: 'no'
+							}
+						]}
+					/> 
+					<RadioField 
+						label="Are you currently taking any medications that could impair your judgement?" 
+						slug="medications" 
+						options={[
+							{
+								label: "Yes", 
+								value: 'yes'
+							}, 
+							{
+								label: 'No', 
+								value: 'no'
+							}
+						]}
+					/> 
+					<RadioField 
+						label="Do you have a physical condition that might impair or prevent your ability to perform any reasonably physically act normally required to care for children?"
+						slug="physical_condition" 
+						options={[
+							{
+								label: "Yes", 
+								value: 'yes'
+							}, 
+							{
+								label: 'No', 
+								value: 'no'
+							}
+						]}
+					/>
+					<TextareaField 
+						label="If you answered yes, please explain."
+						slug="physical_condition_expl"
+					/> 	
+					<hr /> 
+					<InputField
+						label="How did you hear about Kate Care?"
+						slug="referral"
+					/>
+					<InputField
+						label="Were you referred by someone? If yes, who?"
+						slug="referred_by"
+					/>									
+					<hr /> 
+					<CheckboxField 
+						label=""		
+						slug="truth"
+						options={[
+							{
+								label: "I certify that to the best of my ability I have answered truthfully. If any information is found to be false or misrepresenting it will be sufficient cause to cancel my application",
+								value: "true"
+							}
+						]}
+					/>
 				</form> 
 			</div>
 		); 
